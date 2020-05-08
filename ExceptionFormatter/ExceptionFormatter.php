ae55cc93e46099ca50f3749389dfc86e3882165b
Artur Doruch <arturdoruch@interia.pl>
2020-05-08 03:11:40 +0200

    Add ExceptionFormatter tool

---
+++ ExceptionFormatter/ExceptionFormatter.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace ArturDoruch\Tool\ExceptionFormatter;
+
+/**
+ * @author Artur Doruch <arturdoruch@interia.pl>
+ */
+class ExceptionFormatter
+{
+    /**
+     * @var string The path to the application directory, using to shorting trace filename.
+     */
+    private $appDir = '';
+
+    /**
+     * @var bool
+     */
+    private $shortenFilename;
+
+    /**
+     * @var array
+     */
+    private $traceHtmlTemplates = [
+        'list' => '<ol class="exception-trace">%s</ol>',
+        'listItem' => '<li>%s</li>',
+        'function' => '%s%s<b>%s</b><span class="text-muted">(%s)</span>',
+        'class' => '<abbr title="%s">%s</abbr>',
+        'path' => ' in <span class="text-danger">%s<b>%s</b> (line %d)</span>',
+    ];
+
+    /**
+     * @param string $appDir
+     * @param array $traceHtmlTemplates
+     *  - list (string)
+     *  - listItem (string)
+     *  - function (string)
+     *  - class (string)
+     *  - path (string)
+     */
+    public function __construct(string $appDir = '', array $traceHtmlTemplates = [])
+    {
+        if ($appDir) {
+            $this->appDir = rtrim(realpath($appDir), '/') . '/';
+        }
+
+        $this->traceHtmlTemplates = array_merge($this->traceHtmlTemplates, $traceHtmlTemplates);
+    }
+
+    /**
+     * Formats exception properties.
+     *
+     * @param \Throwable $e
+     * @param bool $shortenFilename
+     *
+     * @return array
+     */
+    public function format(\Throwable $e, bool $shortenFilename = true): array
+    {
+        $this->shortenFilename = $shortenFilename;
+
+        return [
+            'message' => $e->getMessage(),
+            'class' => $class = get_class($e),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => $this->getTraceAsHtml($e),
+            'formattedClass' => $this->formatClass($class),
+            'path' => $this->formatPath($e->getFile(), $e->getLine()),
+        ];
+    }
+
+    /**
+     * Gets exception trace formatted to HTML.
+     *
+     * @param \Throwable $e
+     * @param bool $shortenFilename
+     *
+     * @return string
+     */
+    public function getTraceAsHtml(\Throwable $e, bool $shortenFilename = true): string
+    {
+        $this->shortenFilename = $shortenFilename;
+
+        if ($e instanceof \Error) {
+            $e = new ErrorException($e);
+        }
+
+        $exceptionStack = FlattenException::create($e)->toArray();
+
+        if (!$exception = array_shift($exceptionStack)) {
+            return null;
+        }
+
+        $items = '';
+
+        foreach ($exception['trace'] as $trace) {
+            $item = '';
+
+            if ($trace['function']) {
+                $item .= sprintf(
+                    $this->traceHtmlTemplates['function'],
+                    $this->formatClass($trace['class']),
+                    $trace['type'],
+                    $trace['function'],
+                    $this->formatArgs($trace['args'])
+                );
+            }
+
+            if (isset($trace['file']) && isset($trace['line'])) {
+                $item .= $this->formatPath($trace['file'], $trace['line']);
+            }
+
+            $items .= sprintf($this->traceHtmlTemplates['listItem'], $item);
+        }
+
+        return sprintf($this->traceHtmlTemplates['list'], $items);
+    }
+
+    /**
+     * Removes application root directory from the filename.
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    public function shortenFilename(string $filename)
+    {
+        return preg_replace('~^' . $this->appDir . '~', '', $filename);
+    }
+
+    /**
+     * Formats an array as a string.
+     *
+     * @param array $args The argument array
+     *
+     * @return string
+     */
+    private function formatArgs(array $args)
+    {
+        $result = [];
+
+        foreach ($args as $key => $item) {
+            if ('object' === $item[0]) {
+                $formattedValue = sprintf('<em>object</em>(%s)', $this->formatClass($item[1]));
+            } elseif ('array' === $item[0]) {
+                $formattedValue = sprintf('<em>array</em>(%s)', is_array($item[1]) ? $this->formatArgs($item[1]) : $item[1]);
+            } elseif ('null' === $item[0]) {
+                $formattedValue = '<em>null</em>';
+            } elseif ('boolean' === $item[0]) {
+                $formattedValue = '<em>'.strtolower(var_export($item[1], true)).'</em>';
+            } elseif ('resource' === $item[0]) {
+                $formattedValue = '<em>resource</em>';
+            } else {
+                $formattedValue = str_replace("\n", '', $this->escapeHtml(var_export($item[1], true)));
+            }
+
+            $result[] = is_int($key) ? $formattedValue : sprintf("'%s' => %s", $key, $formattedValue);
+        }
+
+        return implode(', ', $result);
+    }
+
+    /**
+     * @param string $file
+     * @param int $line
+     *
+     * @return string
+     */
+    private function formatPath($file, $line)
+    {
+        if ($this->shortenFilename) {
+            $file = $this->shortenFilename($file);
+        }
+
+        $lastDirSeparatorPosition = strrpos($file, DIRECTORY_SEPARATOR) + 1;
+        $path = substr($file, 0, $lastDirSeparatorPosition);
+        $filename = substr($file, $lastDirSeparatorPosition);
+
+        return sprintf($this->traceHtmlTemplates['path'], $path, $filename, $line);
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return string
+     */
+    private function formatClass(string $class)
+    {
+        $parts = explode('\\', $class);
+
+        return sprintf($this->traceHtmlTemplates['class'], $class, array_pop($parts));
+    }
+
+    /**
+     * HTML-encodes a string.
+     *
+     * @param $string
+     * @return string
+     */
+    private function escapeHtml($string)
+    {
+        return htmlspecialchars($string, ENT_COMPAT | ENT_SUBSTITUTE, 'UTF-8');
+    }
+}
---
+++ ExceptionFormatter/FlattenException.php
@@ -0,0 +1,280 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArturDoruch\Tool\ExceptionFormatter;
+
+/**
+ * Wraps a PHP Exception to be able to serialize it.
+ *
+ * Modified by Artur Doruch <arturdoruch@interia.pl>
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class FlattenException
+{
+    /**
+     * @var string The exception class name.
+     */
+    private $class;
+
+    /**
+     * @var string
+     */
+    private $file;
+
+    /**
+     * @var int
+     */
+    private $line;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var int
+     */
+    private $code;
+
+    /**
+     * @var \Throwable
+     */
+    private $previous;
+
+    /**
+     * @var array
+     */
+    private $trace;
+
+    public static function create(\Exception $exception)
+    {
+        $e = new static();
+        $e->setMessage($exception->getMessage());
+        $e->setCode($exception->getCode());
+
+        $e->setTraceFromException($exception);
+        $e->setClass(\get_class($exception));
+        $e->setFile($exception->getFile());
+        $e->setLine($exception->getLine());
+
+        $previous = $exception->getPrevious();
+
+        if ($previous instanceof \Exception) {
+            $e->setPrevious(static::create($previous));
+        } elseif ($previous instanceof \Throwable) {
+            $e->setPrevious(static::create(new ErrorException($previous)));
+        }
+
+        return $e;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $exceptions = [];
+        $exceptionList = array_merge([$this], $this->getAllPrevious());
+
+        /** @var FlattenException $exception */
+        foreach ($exceptionList as $exception) {
+            $exceptions[] = [
+                'message' => $exception->getMessage(),
+                'class' => $exception->getClass(),
+                'trace' => $exception->getTrace(),
+            ];
+        }
+
+        return $exceptions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+
+    public function setClass($class)
+    {
+        $this->class = $class;
+    }
+
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+
+    public function getLine()
+    {
+        return $this->line;
+    }
+
+
+    public function setLine($line)
+    {
+        $this->line = $line;
+    }
+
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+
+    public function getPrevious()
+    {
+        return $this->previous;
+    }
+
+
+    public function setPrevious(self $previous)
+    {
+        $this->previous = $previous;
+    }
+
+
+    public function getAllPrevious()
+    {
+        $exceptions = [];
+        $e = $this;
+        while ($e = $e->getPrevious()) {
+            $exceptions[] = $e;
+        }
+
+        return $exceptions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTrace()
+    {
+        return $this->trace;
+    }
+
+
+    public function setTraceFromException(\Exception $exception)
+    {
+        $this->setTrace($exception->getTrace(), $exception->getFile(), $exception->getLine());
+    }
+
+
+    public function setTrace(array $trace, $file, $line)
+    {
+        $this->trace = [];
+        $this->trace[] = [
+            'namespace' => '',
+            'short_class' => '',
+            'class' => '',
+            'type' => '',
+            'function' => '',
+            'file' => $file,
+            'line' => $line,
+            'args' => [],
+        ];
+        foreach ($trace as $entry) {
+            $class = '';
+            $namespace = '';
+            if (isset($entry['class'])) {
+                $parts = explode('\\', $entry['class']);
+                $class = array_pop($parts);
+                $namespace = implode('\\', $parts);
+            }
+
+            $this->trace[] = [
+                'namespace' => $namespace,
+                'short_class' => $class,
+                'class' => isset($entry['class']) ? $entry['class'] : '',
+                'type' => isset($entry['type']) ? $entry['type'] : '',
+                'function' => isset($entry['function']) ? $entry['function'] : null,
+                'file' => isset($entry['file']) ? $entry['file'] : null,
+                'line' => isset($entry['line']) ? $entry['line'] : null,
+                'args' => isset($entry['args']) ? $this->flattenArgs($entry['args']) : [],
+            ];
+        }
+    }
+
+
+    private function flattenArgs($args, $level = 0, &$count = 0)
+    {
+        $result = [];
+        foreach ($args as $key => $value) {
+            if (++$count > 1e4) {
+                return ['array', '*SKIPPED over 10000 entries*'];
+            }
+            if ($value instanceof \__PHP_Incomplete_Class) {
+                // is_object() returns false on PHP<=7.1
+                $result[$key] = ['incomplete-object', $this->getClassNameFromIncomplete($value)];
+            } elseif (\is_object($value)) {
+                $result[$key] = ['object', \get_class($value)];
+            } elseif (\is_array($value)) {
+                if ($level > 10) {
+                    $result[$key] = ['array', '*DEEP NESTED ARRAY*'];
+                } else {
+                    $result[$key] = ['array', $this->flattenArgs($value, $level + 1, $count)];
+                }
+            } elseif (null === $value) {
+                $result[$key] = ['null', null];
+            } elseif (\is_bool($value)) {
+                $result[$key] = ['boolean', $value];
+            } elseif (\is_int($value)) {
+                $result[$key] = ['integer', $value];
+            } elseif (\is_float($value)) {
+                $result[$key] = ['float', $value];
+            } elseif (\is_resource($value)) {
+                $result[$key] = ['resource', get_resource_type($value)];
+            } else {
+                $result[$key] = ['string', (string) $value];
+            }
+        }
+
+        return $result;
+    }
+
+
+    private function getClassNameFromIncomplete(\__PHP_Incomplete_Class $value)
+    {
+        $array = new \ArrayObject($value);
+
+        return $array['__PHP_Incomplete_Class_Name'];
+    }
+}
